067cf145415031334cbfe0cea675ceaf4f1c8aa7
info700700 <info700700@yandex.ru>
2022-02-09 19:54:26 +0300

    process user input

--- calc.go
+++ calc.go
@@ -3,12 +3,23 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/info700700/calc-go/interpreter"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Error. Invalid argument amount. One argument expected.")
+	if len(os.Args) != 2 {
+		fmt.Println("Ошибка. Неверное количество аргументов. Требуется 1 аргумент.")
 		return
 	}
-	fmt.Println(os.Args[1])
+
+	str := os.Args[1]
+
+	result, err := interpreter.Exec(str)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(result)
 }
