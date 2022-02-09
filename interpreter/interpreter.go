afe79ecdd44de7c01af00c48228c11c1142b7a6b
info700700 <info700700@yandex.ru>
2022-02-09 18:50:31 +0300

    num parsing

--- interpreter/interpreter.go
+++ interpreter/interpreter.go
@@ -1,6 +1,9 @@
 package interpreter
 
-import "strings"
+import (
+	"strconv"
+	"strings"
+)
 
 func Exec(str string) (uint32, error) {
 	parts := strings.Split(str, "+")
@@ -18,7 +21,7 @@ func convertStrsToUint32s(strs []string) ([]uint32, error) {
 	var nums []uint32
 
 	for _, str := range strs {
-		num, err := convertStrToUint32(str)
+		num, err := convertStrWithSpacesToUint32(str)
 		if err != nil {
 			return nil, err
 		}
@@ -29,8 +32,15 @@ func convertStrsToUint32s(strs []string) ([]uint32, error) {
 	return nums, nil
 }
 
-func convertStrToUint32(str string) (uint32, error) {
-	return 0, nil
+func convertStrWithSpacesToUint32(str string) (uint32, error) {
+	trimmedStr := strings.TrimSpace(str)
+
+	n, err := strconv.ParseUint(trimmedStr, 10, 32)
+	if err != nil {
+		return 0, err
+	}
+
+	return uint32(n), nil
 }
 
 func sumUint32s(nums []uint32) uint32 {
